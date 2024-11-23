953ee00f20555beba42bc61ac4e4e2d0c316347a
Mahendra Dhamala <rijalmpd1234@gmail.com>
2024-11-23 20:26:41 +0545

    feature : add models and migrations

---
+++ backend/chapter_generator/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+# Base model to include created_at and updated_at fields
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# Reference Type Model
+class StypeReference(BaseModel):
+    REFERENCE_TYPES = [
+        ("professional", "Professional"),
+        ("personal", "Personal"),
+        ("technical", "Technical"),
+        ("philosophical", "Philosophical"),
+        ("fun", "Fun"),
+    ]
+    type = models.CharField(max_length=20, choices=REFERENCE_TYPES, unique=True)
+
+    def __str__(self):
+        return self.get_type_display()  # Returns the display name of the selected type
+
+
+# Chapter Model
+class Chapter(BaseModel):
+    CHAPTER_STATUS = [
+        ("draft", "Draft"),
+        ("completed", "Completed"),
+    ]
+
+    title = models.CharField(max_length=100)
+    outlines = models.JSONField()  # Stores chapter outlines in JSON format
+    status = models.CharField(max_length=10, choices=CHAPTER_STATUS, default="draft")
+    reference_type = models.ForeignKey(StypeReference, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Resources(BaseModel):
+    RESOURCE_TYPES = [
+        ("blog", "Blog"),
+        ("pdf", "Pdf"),
+        ("research_paper", "Research Paper"),
+    ]
+    resource_type = models.CharField(max_length=50, choices=RESOURCE_TYPES)
+    source_url = models.CharField(max_length=100, null=True)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    content_extracted = models.TextField()  # content to store in case we need it
